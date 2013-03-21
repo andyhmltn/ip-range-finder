cd4e7269fc06040980de1cf19a1b6b676846b0e9
Andy Hamilton <andyhmltn@gmail.com>
2013-03-21 19:33:08 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+ip-range-find
+==================
+
+Someone on an internet forum requested this so I made it. TODO: Convert it to a usable class.
+
+Usage:
+
+`python ip_range_finder.py`
---
+++ ip_range_finder.py
@@ -0,0 +1,33 @@
+from struct import *
+from socket import *
+
+def ip_range_search(start, end, search):
+
+	found     = False
+	search_ip = gethostbyname(search)
+
+	try:
+		for ip in xrange(unpack('!I',inet_pton(AF_INET,start))[0],unpack('!I',inet_pton(AF_INET,end))[0]):
+			current_ip = inet_ntop(AF_INET, pack('!I', ip))
+
+			if current_ip == search_ip:
+				found = True
+	except error:
+		print('Invalid IP Address!')
+
+	return found
+
+start_ip = raw_input('Please Enter The First IP: ')
+end_ip   = raw_input('Please Enter The Last IP: ')
+
+domain   = raw_input('Please Enter The Domain Name: ')
+
+print('Please wait, finding domain in IP address... (This may take a while)')
+
+if ip_range_search(start_ip, end_ip, domain):
+	print('IP in given range!')
+else:
+	print('IP not in given range')
+
+
+
